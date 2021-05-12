e29e5ee059e7afaec777d5a60b525c61f7bdeccf
Jess Roque <jessroque16@gmail.com>
2021-05-12 20:14:53 +0800

    commented user control

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,32 +2,32 @@
 
 class Pages extends Controller{
     public function __construct(){
-        if (!isLoggedIn()) {
-            redirect('users/login');
-        }
+        // if (!isLoggedIn()) {
+        //     redirect('users/login');
+        // }
     }
     
     public function index(){
-        if (isLoggedIn()) {
-            if (userType() == "Admin") {
-                redirect('admin/dashboard');
-            } elseif (userType() == "Alumni") {
-                redirect('pages/home');
-            } elseif (userType() == "Content Creator") {
-                redirect('admin/dashboard');
-            }
-        }
+        // if (isLoggedIn()) {
+        //     if (userType() == "Admin") {
+        //         redirect('admin/dashboard');
+        //     } elseif (userType() == "Alumni") {
+        //         redirect('pages/home');
+        //     } elseif (userType() == "Content Creator") {
+        //         redirect('admin/dashboard');
+        //     }
+        // }
 
 
     }
 
     public function home() {
-        if (isLoggedIn()) {
-            $data = [
-                'title' => 'Homepage'
-            ]; 
-            $this->view('pages/home' ,$data);
-        }
+        // if (isLoggedIn()) {
+        //     $data = [
+        //         'title' => 'Homepage'
+        //     ]; 
+        //     $this->view('pages/home' ,$data);
+        // }
 
     }
 
