a9748bb055063fc142f24a7d66e186aafff0509d
GitNiel <dpagayon15@gmail.com>
2021-05-15 18:30:24 +0800

    home navbar work

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -3,10 +3,10 @@
 <main class="home">
         <section class="heroBox">
             <div class="container">
-              <!--   <h1 class="heroBoxText">PUP Institute of Technology <br> Alumni Information System</h1> -->
-                <h1>Welcome, <?php echo $_SESSION['name'] ?></h1>
+                <h1 class="heroBoxText">PUP Institute of Technology <br> Alumni Information System</h1>
+                <!-- <span>Alumni Information System</span> -->
             </div>
         </section>
     </main>
-
+    
 <?php require APPROOT . '/views/inc/footer.php'; ?>
