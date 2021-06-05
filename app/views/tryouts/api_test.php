bda64feb2c267cf695ad45b2d8ef141f08cb7d7d
Jess Roque <jessroque16@gmail.com>
2021-06-05 23:17:40 +0800

    qwe

---
+++ app/views/tryouts/api_test.php
@@ -0,0 +1,22 @@
+<a href="<?php echo URLROOT;?>/admin/dashboard">Run it back!</a>
+   
+    <h1>API TESTING GOD SSEJ ARGUX BUBU</h1>
+    <br>
+    <h3>A new era of programming, the era of the establisment of REST API</h3>
+
+    <button id="api-news">API NEWS</button>
+    <button id="api-events">API EVENTS</button>
+
+
+  <script>
+        const apiNews = document.getElementById('api-news');
+        const apiEvents = document.getElementById('api-events');
+
+        apiNews.addEventListener('click', function(){
+            fetch('http://localhost/pupiais/api/posts/read').then(res => res.json()).then(data => console.log(data))
+        })
+
+        apiEvents.addEventListener('click', function(){
+            fetch('http://localhost/pupiais/api/events/read').then(res => res.json()).then(data => console.log(data))
+        })
+ </script>
