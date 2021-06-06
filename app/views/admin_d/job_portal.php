3308848175cf7ea23904f04595e02e947ccddfb5
Jess Roque <jessroque16@gmail.com>
2021-06-06 16:46:53 +0800

    qwe

--- app/views/admin_d/job_portal.php
+++ app/views/admin_d/job_portal.php
@@ -56,11 +56,7 @@
                                 <td><div class="option" tabindex="0">
                                     <span class="optionSpan icon">&#8942</span>
                                     <div class="optionModal">
-<<<<<<< HEAD
-                                        <a href="<?php echo URLROOT;?>/job_portals/single/<?php echo $job->id?>">View</a>
-=======
                                         <a href="<?php echo URLROOT ?>/job_portals/single/<?php echo $job->id?>">View</a>
->>>>>>> 3cb4c99bc1e9cd135e5c5ffc115e47cfba593c01
                                         <a href="<?php echo URLROOT;?>/job_portals/edit/<?php echo $job->id?>">
                                             <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M15.8233 1.67676L18.3233 4.17676L16.4175 6.08342L13.9175 3.58342L15.8233 1.67676Z" fill="black" fill-opacity="0.87"/>
