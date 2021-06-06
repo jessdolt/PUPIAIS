751051a1cc20dc5d7029d0c585e7bf9e14faf81b
Jess Roque <jessroque16@gmail.com>
2021-06-06 14:31:45 +0800

    qwe

--- app/views/prac/prac.php
+++ app/views/prac/prac.php
@@ -1,30 +1,8 @@
-<?php
-$servername = "us-cdbr-east-04.cleardb.com";
-$username = "b9e84a4bdcd7c6";
-$password = "2891ffd3";
-$dbname ='heroku_ec03275637a73ae';
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+<?php 
+    $db = new Database;
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-
-$sql = "SELECT * FROM users";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
+    $db->query('SELECT * FROM users');
+    $row = $db->resultSet();
     array_print($row);
-  }
-} else {
-  echo "0 results";
-}
 
-
-
-?>
+?> 
