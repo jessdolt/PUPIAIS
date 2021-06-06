a189a0b1fd09461bd00d2171260686e55c728b81
Jess Roque <jessroque16@gmail.com>
2021-06-06 15:00:53 +0800

    qwe

--- app/views/prac/prac.php
+++ app/views/prac/prac.php
@@ -1,2 +1,23 @@
-<?php array_print($data)?>
 
+
+
+<?php 
+    $db = new Database;
+
+    $db->query('SELECT * FROM users');
+    $row = $db->resultSet();
+    array_print($row);
+
+    
+?> 
+
+
+<?php 
+    $db = new Database;
+
+    $db->query('SELECT * FROM alumni');
+    $row = $db->resultSet();
+    array_print($row);
+
+    
+?> 
