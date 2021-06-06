1d5ebf15a1625f5fcccf0eb9dbd7ab62e412c5b4
Jess Roque <jessroque16@gmail.com>
2021-06-06 14:04:29 +0800

    qwe

--- app/config/config.php
+++ app/config/config.php
@@ -18,7 +18,7 @@
     define('CLOGOROOT', dirname(dirname(dirname(__FILE__))). '\public\company_logo\\');
     
     //Url Root
-    define('URLROOT', 'http://localhost/pupiais');
+    define('URLROOT', 'https://pupiais.herokuapp.com');
 
     //https://pupiais.herokuapp.com
     
