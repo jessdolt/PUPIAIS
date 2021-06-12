dd0c1d0dc8d91834b7957808182ef8576899d9e0
Jess Roque <jessroque16@gmail.com>
2021-06-12 17:42:01 +0800

    heroku up

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php 
-    //DB Params
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '12345');
-    define('DB_NAME', 'pupiais');
+    //LOCAL DB (PHPMYADMIN)
+    // define('DB_HOST', 'localhost');
+    // define('DB_USER', 'root');
+    // define('DB_PASS', '12345');
+    // define('DB_NAME', 'pupiais');
 
-    //https://remotemysql.com/login.php
-    // define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
-    // define('DB_USER', 'b9e84a4bdcd7c6');
-    // define('DB_PASS', '2891ffd3');
-    // define('DB_NAME', 'heroku_ec03275637a73ae');
+    //REMOTE DB (LIVE) 
+    define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
+    define('DB_USER', 'b014f8dc846f63');
+    define('DB_PASS', '1493f4d1');
+    define('DB_NAME', 'heroku_c03df737b159067');
 
     //App Root
     define('APPROOT',dirname(dirname(__FILE__)));
@@ -17,10 +17,10 @@
     define('CLOGOROOT', dirname(dirname(dirname(__FILE__))). '\public\company_logo\\');
     
     //Url Root
-    define('URLROOT', 'http://localhost/pupiais'); 
+    // define('URLROOT', 'http://localhost/pupiais'); 
     //http://localhost/pupiais
 
-    //define('URLROOT', 'https://pupiais.herokuapp.com'); 
+    define('URLROOT', 'https://pupiais.herokuapp.com'); 
     //https://pupiais.herokuapp.com
     
     //Site Name
