715e94b959b83f448f090dee2764d51603864274
Jess Roque <jessroque16@gmail.com>
2021-07-07 12:43:29 +0800

    last commit

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php 
     //LOCAL DB (PHPMYADMIN)
-    // define('DB_HOST', 'localhost');
-    // define('DB_USER', 'root');
-    // define('DB_PASS', '12345');
-    // define('DB_NAME', 'pupiais');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '12345');
+    define('DB_NAME', 'pupiais');
 
     //REMOTE DB (LIVE) 
-    define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
-    define('DB_USER', 'bf4e22d91874f8');
-    define('DB_PASS', '43fac3f2');
-    define('DB_NAME', 'heroku_c1a5f6d46fe9dd8');
+    // define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
+    // define('DB_USER', 'bf4e22d91874f8');
+    // define('DB_PASS', '43fac3f2');
+    // define('DB_NAME', 'heroku_c1a5f6d46fe9dd8');
 
     //for presentation
     // define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
@@ -17,22 +17,16 @@
     // define('DB_PASS', 'd4a05ee7');
     // define('DB_NAME', 'heroku_84c25b156483496');
 
-    //REmote mysql phpmyadmin
-    // define('DB_HOST', 'remotemysql.com');
-    // define('DB_USER', '9GFORLkQnc');
-    // define('DB_PASS', 'Eu9w3bOVNq');
-    // define('DB_NAME', '9GFORLkQnc');
-
     //App Root
     define('APPROOT',dirname(dirname(__FILE__)));
     define('IMAGEROOT', dirname(dirname(dirname(__FILE__))). '\public\uploads\\');
     define('CLOGOROOT', dirname(dirname(dirname(__FILE__))). '\public\company_logo\\');
     
     //Url Root
-    //define('URLROOT', 'http://localhost/pupiais'); 
+    define('URLROOT', 'http://localhost/pupiais'); 
     //http://localhost/pupiais
 
-    define('URLROOT', 'https://pupiais.herokuapp.com'); 
+    //define('URLROOT', 'https://pupiais.herokuapp.com'); 
     //https://pupiais.herokuapp.com
     
     //Site Name
